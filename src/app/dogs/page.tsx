f6b9f4d3ab0c89c055801dfd9397e0274eef434c
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-12-04 16:20:26 -0500

    pass along the "true" card width to the cards

--- src/app/dogs/page.tsx
+++ src/app/dogs/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import React, { useState } from "react";
 import { getDogs } from "./dogs_data";
 import { DogCard } from "./DogCard";
 import { MasonryLayout } from "@/components/ui/masonry/MasonryLayout";
 
 const dogs = getDogs();
 export default function Dogs() {
+  const desiredColumnWidth = 200;
+  const [cardWidth, setCardWidth] = useState(desiredColumnWidth);
+
   return (
     <div>
       <h1 className="text-4xl font-bold mb-8 text-center">
         Collection of My Furry Friends
       </h1>
-      <MasonryLayout columnWidth={200}>
+      <MasonryLayout
+        columnWidth={desiredColumnWidth}
+        setCardWidth={setCardWidth}
+      >
         {dogs.map((dog) => (
-          <DogCard width={200} {...dog} />
+          <DogCard width={cardWidth} {...dog} />
         ))}
       </MasonryLayout>
     </div>
