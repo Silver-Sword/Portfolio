152c03ad08c13d499115a91d2655ccd5c2530824
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-12-05 16:40:44 -0500

    add 10 more favorites

--- src/app/about/favorites.ts
+++ src/app/about/favorites.ts
@@ -1,3 +1,5 @@
+import { link } from "fs";
+
 const IMAGE_URL = "/favorites-images";
 export const FAVORITES_DATA = [
   {
@@ -34,16 +36,17 @@ export const FAVORITES_DATA = [
     },
   },
   {
-    name: "Cradle Series",
-    category: "novel",
-    link: "https://www.goodreads.com/series/192821-cradle",
+    name: "Control",
+    category: "video game",
+    link: "https://store.steampowered.com/app/870780/Control_Ultimate_Edition/",
     imageData: {
-      url: `${IMAGE_URL}/cradle.jpg`,
-      pixelWidth: 667,
-      pixelHeight: 1000,
-      alt: "Cradle Series",
+      url: `${IMAGE_URL}/control.jpg`,
+      pixelWidth: 1024,
+      pixelHeight: 576,
+      alt: "Control",
     },
   },
+
   {
     name: "Noita",
     category: "video game",
@@ -67,14 +70,14 @@ export const FAVORITES_DATA = [
     },
   },
   {
-    name: "Control",
-    category: "video game",
-    link: "https://store.steampowered.com/app/870780/Control_Ultimate_Edition/",
+    name: "Cradle Series",
+    category: "novel",
+    link: "https://www.goodreads.com/series/192821-cradle",
     imageData: {
-      url: `${IMAGE_URL}/control.jpg`,
-      pixelWidth: 1024,
-      pixelHeight: 576,
-      alt: "Control",
+      url: `${IMAGE_URL}/cradle.jpg`,
+      pixelWidth: 667,
+      pixelHeight: 1000,
+      alt: "Cradle Series",
     },
   },
   {
@@ -111,6 +114,17 @@ export const FAVORITES_DATA = [
     },
   },
   {
+    name: "Tries",
+    category: "algorithm",
+    link: "https://en.wikipedia.org/wiki/Trie",
+    imageData: {
+      url: `${IMAGE_URL}/tries.png`,
+      pixelWidth: 284,
+      pixelHeight: 178,
+      alt: "Tries",
+    },
+  },
+  {
     name: "Ready Player One",
     category: "novel",
     link: "https://www.goodreads.com/book/show/9969571-ready-player-one",
@@ -122,17 +136,6 @@ export const FAVORITES_DATA = [
     },
   },
 
-  {
-    name: "Tries",
-    category: "algorithm",
-    link: "https://en.wikipedia.org/wiki/Trie",
-    imageData: {
-      url: `${IMAGE_URL}/tries.png`,
-      pixelWidth: 284,
-      pixelHeight: 178,
-      alt: "Tries",
-    },
-  },
   {
     name: "Heavy-Light Decomposition (HLD)",
     category: "algorithm",
@@ -164,6 +167,116 @@ export const FAVORITES_DATA = [
       pixelWidth: 1298,
       pixelHeight: 434,
       alt: "Dynamic Connectivity",
+    },
+  },
+  {
+    name: "Firewatch",
+    category: "video game",
+    link: "https://store.steampowered.com/app/383870/Firewatch/",
+    imageData: {
+      url: `${IMAGE_URL}/firewatch.jpg`,
+      pixelWidth: 616,
+      pixelHeight: 353,
+      alt: "Firewatch",
+    },
+  },
+  {
+    name: "ADOM",
+    category: "video game",
+    link: "https://store.steampowered.com/app/333300/ADOM_Ancient_Domains_Of_Mystery/",
+    imageData: {
+      url: `${IMAGE_URL}/adom.jpg`,
+      pixelWidth: 800,
+      pixelHeight: 500,
+      alt: "ADOM",
+    },
+  },
+  {
+    name: "Ender's Game",
+    category: "novel",
+    link: "https://www.goodreads.com/book/show/375802.Ender_s_Game",
+    imageData: {
+      url: `${IMAGE_URL}/enders-game.jpg`,
+      pixelWidth: 181,
+      pixelHeight: 278,
+      alt: "Ender's Game",
+    },
+  },
+  {
+    name: "The Martian",
+    category: "novel",
+    link: "https://www.goodreads.com/book/show/18007564-the-martian",
+    imageData: {
+      url: `${IMAGE_URL}/the-martian.jpg`,
+      pixelWidth: 313,
+      pixelHeight: 475,
+      alt: "The Martian",
+    },
+  },
+  {
+    name: "Uncharted 4: A Thief's End",
+    category: "video game",
+    link: "https://www.playstation.com/en-us/games/uncharted-4-a-thiefs-end/",
+    imageData: {
+      url: `${IMAGE_URL}/uncharted-4.jpg`,
+      pixelWidth: 300,
+      pixelHeight: 168,
+      alt: "Uncharted 4: A Thief's End",
+    },
+  },
+  {
+    name: "Return of the Obra Dinn",
+    category: "video game",
+    link: "https://store.steampowered.com/app/653530/Return_of_the_Obra_Dinn/",
+    imageData: {
+      url: `${IMAGE_URL}/obra-dinn.png`,
+      pixelWidth: 1600,
+      pixelHeight: 900,
+      alt: "Return of the Obra Dinn",
+    },
+  },
+  {
+    name: "Superhot: Mind Control Delete",
+    category: "video game",
+    link: "https://store.steampowered.com/app/690040/SUPERHOT_MIND_CONTROL_DELETE/",
+    imageData: {
+      url: `${IMAGE_URL}/superhot.jpg`,
+      pixelWidth: 1920,
+      pixelHeight: 1080,
+      alt: "Superhot: Mind Control Delete",
+    },
+  },
+  {
+    name: "Inscryption",
+    category: "video game",
+    link: "https://store.steampowered.com/app/1092790/Inscryption/",
+    imageData: {
+      url: `${IMAGE_URL}/inscryption.jpg`,
+      pixelWidth: 300,
+      pixelHeight: 168,
+      alt: "Inscryption",
+    },
+  },
+  {
+    name: "Unavowed",
+    category: "video game",
+    link: "https://store.steampowered.com/app/336140/Unavowed/",
+    imageData: {
+      url: `${IMAGE_URL}/unavowed.png`,
+      pixelWidth: 400,
+      pixelHeight: 250,
+      alt: "Unavowed",
+    },
+  },
+  {
+    name: "Quantum Break",
+    category: "video game",
+    link: "https://store.steampowered.com/app/474960/Quantum_Break/",
+    imageData: {
+      url: `${IMAGE_URL}/quantum-break.jpg`,
+      pixelWidth: 740,
+      pixelHeight: 1008,
+      alt: "Quantum Break",
     },
   },
 ];
