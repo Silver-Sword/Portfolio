3d5d8bcce486f1a4188b492a610a8142eb9f4c75
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-12-13 13:52:00 -0500

    change font weights and spacing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
   return (
     <div>
       <title>Projects - Chris Gittings</title>
-      <h1 className="text-4xl font-bold mb-8 text-center">My Projects</h1>
+      <h1 className="text-4xl font-semibold mb-8 text-center">My Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
           <ProjectCard key={project.id} {...project} />
